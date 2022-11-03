f8de4227a0b814a50a7a61eca55b2ffd8611a6cb
pixelclaws <113621234+pixelclaws@users.noreply.github.com>
2022-11-03 01:51:16 +0000

    Diabetes Hub
    A brief overview of what diabetes is and facts around this area.

---
+++ game.js
@@ -0,0 +1,55 @@
+var rsvp = prompt ("Ready for a diabetes quiz? (yes or no)")
+
+if (rsvp == "yes") {
+    alert ("Amazing - Here we go!")
+    // var response = prompt ("Are you sure?")
+
+    // if (rsvp == "yes"){
+    //     alert ("Let's go!")
+    // }
+
+    // else {
+    //     alert ("Here we go!")
+    // }
+}
+
+else if (rsvp == "no") {
+    alert ("Sorry to hear that.We'll proceed anyways")
+}
+
+else {
+    alert ("I didn't quite catch that but welcome to the quiz page - Have fun!")
+}
+
+// var newparagraph = document.createElement('p'); 
+// newparagraph.innerText="Yay!"; 
+// newparagraph.style.fontSize="18px"; 
+// newparagraph.style.fontFamily="sans-serif"; 
+// newparagraph.style.width="100px"; 
+// newparagraph.style.border="1px solid orange";
+// newparagraph.style.padding="30px";
+// newparagraph.style.alignContent="center";
+// // append 
+// document.body.appendChild(newparagraph);
+
+// // element.addEventListener (event, function )
+// newparagraph.addEventListener('click',
+//   function(event) {
+//       console.log(event);
+//       console.log(event.target); 
+//       event.target.innerText="I was clicked!!";
+
+//   }
+// )
+// newparagraph.addEventListener('mouseenter',
+//   function(event){
+//     console.log("mouse enter");
+//     event.target.style.color="purple";
+//   }
+//   )
+//   newparagraph.addEventListener('mouseleave',
+//   function(event){
+//     console.log("mouse leave");
+//     event.target.style.border="2px solid purple";
+//   }
+//   )
